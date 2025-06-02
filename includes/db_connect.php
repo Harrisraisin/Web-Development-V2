a0b3d498847db1372293b91935b323f3c1eb1b94
Harrisraisin <61838530+Harrisraisin@users.noreply.github.com>
2025-06-02 17:44:12 +0100

    update password

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost:3306";
 $dbname = "s4303980_social_media";
 $username = "s4303980_dbuser";
-$password = "password";
+$password = "91#tf08jH";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
