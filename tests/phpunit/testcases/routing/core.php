087dec71d9cef8dfb509ed054a8b14dc157567b1
Harrisraisin <61838530+Harrisraisin@users.noreply.github.com>
2025-06-02 21:01:47 +0100

    lots

---
+++ tests/phpunit/testcases/routing/core.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @group core
+ * @group routing
+ */
+class BP_Tests_Routing_Core extends BP_UnitTestCase {
+	protected $old_current_user = 0;
+	protected $permalink_structure = '';
+
+	public function set_up() {
+		parent::set_up();
+
+		$this->old_current_user = get_current_user_id();
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'subscriber' ) ) );
+		$this->permalink_structure = get_option( 'permalink_structure', '' );
+	}
+
+	public function tear_down() {
+		wp_set_current_user( $this->old_current_user );
+		$this->set_permalink_structure( $this->permalink_structure );
+
+		parent::tear_down();
+	}
+
+	function test_wordpress_page() {
+		$this->set_permalink_structure( '/%postname%/' );
+		$this->go_to( '/' );
+		$this->assertEmpty( bp_current_component() );
+	}
+}
